c6cd0792df473b7047a3a6097a31947e1da05e1d
cryptorich1 <jevico1@gmail.com>
2022-12-02 08:17:35 -0800

    ALX python programming 002

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
 
-def magic_calculation(a, b, c):
-    from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
     if a < b:
-        return (c)
-    if b < c:
-        return (a + b)
-    else:
-        return ((a * b) - c)
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+            return (c)
+        else:
+            return sub(a, b)
