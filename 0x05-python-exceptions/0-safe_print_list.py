7d3baf4c2f6f437e79155ef7043459ffa83286f6
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2022-12-20 21:31:40 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     valid_num = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i], "\n")
-            valid_num += 1
+            print("{}".format(my_list[i], end="")
+                  valid_num += 1
         except (IndexError, ValueError, TypeError):
-            break
+                  break
     print("")
     return (valid_num)
