9eaf73409dfc91ae0dfc247c1faebbf2a4e21709
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2022-12-20 19:43:51 +0000

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Square defines a square"""
+
+
+class Square:
+  """This class define a square.
+  This class has no puble attributes.
+  """
+  def __init__(self, size=0):
+     """This method initiates a square.
+        Args:
+            size (int): This defines the size of the square.
+        """
+      self.__size= size
+    
