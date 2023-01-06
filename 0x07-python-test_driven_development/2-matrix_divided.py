afe8cec79e1d912f7950720620f2979cfde01773
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-01-06 16:38:16 +0000

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,8 @@
 This is the "2-matrix_divided" module.
 The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
+
+
 def matrix_divided(matrix, div):
     """Divides all elements in the matrix by div"""
     if type(matrix) is not list:
@@ -19,7 +21,8 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for i in l:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists)of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
