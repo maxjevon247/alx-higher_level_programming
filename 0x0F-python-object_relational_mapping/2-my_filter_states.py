23e47eea3da1853b6aa55c30cf176627f0a802dd
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-03-25 00:51:22 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
-"""
-takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument
-"""
-import MySQLdb
-from sys import argv
+''' script for task 2
+    script should take 4 arguments: mysql username,
+    mysql password, database name and state name searched
+'''
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
+import MySQLdb
+import sys
+
+
+def list_with_name():
+    ''' displays all values in the states table in hbtn db where name
+        matches the argument passed to the script
+    '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = 3306
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name='{:s}'\
-            ORDER BY states.id".format(argv[4])
-            cur.execute(sql)
-            rows = cur.fetchall()
-            for row in rows:
-                if row[1] == argv[4]:
-                    print(row)
-                cur.close()
-                db.close()
+    cur.execute(('SELECT * FROM states WHERE BINARY name = \'{}\'\
+                 ORDER BY id ASC;').format(state_name))
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+
+    if result:
+        for row in result:
+            print(row)
+
+
+if __name__ == '__main__':
+    list_with_name()
