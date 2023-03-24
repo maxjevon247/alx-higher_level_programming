9109de185db77703d2bb815606c32bb5e224162c
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-03-25 00:18:05 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""
-lists all State objects from the database hbtn_0e_6_usa
-"""
-import MySQLdb
-from sys import argv
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
+'''task 10 script'''
+
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
-if __name__ == "__main__":
-    engine = create_engine('mysqldb://{}:{}@localhost/{}'.format
-            (argv[1], argv[2],, argv[3], pool_pre_ping=True
-    Base.metadata.create_all(engine)
 
-    session = Session(engine)
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = '3306'
 
-    flag = 0
-    for state in session.query(State).filter_by(name=argv[4])\
-            .order_by(State.id).all():
-            print("{}".format(state.id))
-            flag = 1
-    if flag != 1:
-        print("Not found")
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    local_session = Session()
+    result = local_session.query(State).filter(
+                            State.name.like(state_name)
+                            ).first()
+    local_session.close()
+    engine.dispose()
 
+    if result:
+        print(result.id)
+    else:
+        print('Not found')
