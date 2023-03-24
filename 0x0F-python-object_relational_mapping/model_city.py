e2e1aa80a16c87e715c2a4b4c5eeb5e272af3d97
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-03-25 00:26:39 +0100

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,18 +2,20 @@
 """ Create City class """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
 Base = declarative_base()
-
 class City(Base):
-    """clase City"""
+    '''City model for my db'''
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, primary_key=True, auto_increament=True)
+    id = Column(Integer, unique=True, nullable=False,
+                autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id Column(Integer, ForeignKey("states.id"), nullable=False
-
-class State(Base):
-    """class City"""
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    
+""" class State(Base):
+    """ """class City""" """"
     __table__ = 'state'
     id = Column(Integer, nullable=False, primary_key=True, auto_increment=True)
     name = Column(String(128), nullable=False)
+"""
