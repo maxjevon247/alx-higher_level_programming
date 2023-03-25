a8f3ef7530dc78cc2cb1e64e842cf37c56f49c6b
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-03-25 01:13:07 +0100

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-lists all State objects from the database hbtn_0e_6_usa
-"""
-import MySQLdb
-from sys import argv
-from sqlalchemy import (create_engine)
-from model_state import Base, State
+'''script for task 11'''
+
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import update
+import sys
 
 
 if __name__ == '__main__':
