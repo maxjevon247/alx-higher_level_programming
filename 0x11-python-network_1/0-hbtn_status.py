193be7cd58a507a37d2d360ed0251b309c85d80e
cryptorich1 <jevico1@gmail.com>
2023-04-04 08:24:57 -0700

    fetch a webpage

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status """
-import urllib.request as request
 
 if __name__ == "__main__":
+    import urllib.request as request
     with urllib.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
