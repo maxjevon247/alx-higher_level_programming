856df82bc653ef7e66588f97e50abf8d87bfe0fc
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-04-26 23:03:23 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 //A script that get webpage content and store it in a file
 
-cons fs = require('fs');
+const fs = require('fs');
 const request = require('request');
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]))
